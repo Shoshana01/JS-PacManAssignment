f406cbb4926d84c0e1f8c26fcea7bfa72598bbd0
Shoshana01 <shanabjacobs@gmail.com>
2021-06-20 22:16:46 -0400

    functions & return values

---
+++ functions.js
@@ -0,0 +1,17 @@
+// named function
+// function eatDot(){
+//     console.log("MUNCH!");
+// }
+
+// anonymous function
+// here we assign the function to a variable instead of giving it a name directly
+// let eatDot = function() {
+//     console.log("MUNCH!");
+// }
+
+// arrow function (also anonymous)
+let eatDot = () => {
+    console.log("MUNCH!");
+}
+
+console.log (eatDot())
